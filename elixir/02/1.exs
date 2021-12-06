6c5918226f95f4a47204358281a28496fe407ed2
Nate Kresge <kresge@gmail.com>
2021-12-05 19:59:58 -0800

    Solve day 02 part 1

---
+++ elixir/02/1.exs
@@ -0,0 +1,25 @@
+defmodule Solution do
+  @moves %{
+   forward: [1, 0],
+   backward: [-1, 0],
+   up: [0, -1],
+   down: [0, 1],
+  }
+
+  def main do
+    IO.stream(:stdio, :line)
+    |> Stream.filter(fn s -> s != "" end)
+    |> Stream.map(&String.slice(&1, 0..-2))
+    |> Stream.map(&String.split(&1))
+    |> Stream.map(fn([dir, mag]) -> Enum.map(@moves[String.to_atom(dir)], &(String.to_integer(mag) * &1)) end)
+    |> Stream.scan([0, 0, 0], fn
+      [mag, d_aim], [x, y, aim] -> [x + mag, y + (mag * aim), aim + d_aim]
+    end)
+    |> Enum.at(-1)
+    |> Enum.take(2)
+    |> Enum.product
+    |> IO.inspect()
+  end
+end
+
+Solution.main()
